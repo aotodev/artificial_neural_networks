c500274d3ea6260738abf46efdea7a901c0f7ae1
aotodev <aotodev@gmail.com>
2023-07-09 11:02:44 +0200

    changes ANSI colors for cmd prompt

--- core.hpp
+++ core.hpp
@@ -1,5 +1,9 @@
 #pragma once
 
+#if defined(__WIN32__) || defined(_WIN32) || defined(__CYGWIN32__)
+#define APP_WINDOWS
+#endif
+
 #if !defined(APP_COMPILER_GNUC) && !defined(APP_COMPILER_CLANG) && !defined(APP_COMPILER_MSVC)
 
 #if defined(__clang__)
@@ -33,14 +37,26 @@
 #endif
 
 #if VERBOSITY > 0
+#ifdef APP_WINDOWS
+#define LOG_INFO(message, ...) printf(message "\n" __VA_OPT__(,) __VA_ARGS__);
+#else
 #define LOG_INFO(message, ...) printf("\033[0;32m" message "\033[0m\n" __VA_OPT__(,) __VA_ARGS__);
+#endif // windows
+
 #else
 #define LOG_INFO(...)
 #endif
 
 #define LOG(message, ...) printf(message "\n" __VA_OPT__(,) __VA_ARGS__);
+
+#ifdef APP_WINDOWS
+#define LOG_WARN(message, ...) printf(message "\n" __VA_OPT__(,) __VA_ARGS__);
+#define LOG_ERROR(message, ...) printf(message "\n" __VA_OPT__(,) __VA_ARGS__);
+#else
+
 #define LOG_WARN(message, ...) printf("\033[0;33m" message "\033[0m\n" __VA_OPT__(,) __VA_ARGS__);
 #define LOG_ERROR(message, ...) printf("\033[0;97;101m" message "\033[0m\n" __VA_OPT__(,) __VA_ARGS__);
+#endif // windows
 
 
 //////////////////////////////////--TYPES--//////////////////////////////////////////////
